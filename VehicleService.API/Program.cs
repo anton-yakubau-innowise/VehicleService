f4eea090bdc522e506fc3f2650e8ac6920c40da5
Anton Jakubau <170anton@gmail.com>
2026-02-18 15:38:38 +0100

    Set listening port 8080 only http2

--- VehicleService.API/Program.cs
+++ VehicleService.API/Program.cs
@@ -28,7 +28,7 @@ try
     {
         options.ListenAnyIP(8080, listenOptions =>
         {
-            listenOptions.Protocols = HttpProtocols.Http1AndHttp2;
+            listenOptions.Protocols = HttpProtocols.Http2;
         });
     });
 
